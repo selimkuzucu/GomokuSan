f4c96174af05392ea130b5bc03d84b2ab92113e0
Selim “Slim Jim” Kuzucu <56355561+selimanratar@users.noreply.github.com>
2021-02-16 20:16:33 +0300

    Created the evaulation functions dir

---
+++ State Evaluation Functions/calculate.cpp
@@ -0,0 +1,32 @@
+long int calculate(int con, int blocks, bool isCurr, bool emptyFound = false){
+    if(blocks == 2 && con < 5){
+        return 0;
+    }
+    
+    else{
+        if(con >= 5){
+            if(emptyFound){
+                return 8000;
+            }
+            return 100000;
+        }
+        vector<int> conScore {2, 5, 1000, 10000};
+        vector<double> blockCountScore {0.5, 0.6, 0.01, 0.25};
+        vector<double> notCurrScore {1, 1, 0.2, 0.15};
+        vector<double> emptySpaceScore {1, 1.2, 0.9, 0.4};
+        
+        int conID = con-1;
+        double value = conScore[conID];
+        if(blocks == 1){
+            value *= blockCountScore[conID];
+        }
+        if (!isCurr){
+            value *= notCurrScore[conID];
+        }
+        if (emptyFound){
+            value *= emptySpaceScore[conID];
+        }
+        int ret_val = value;
+        return ret_val;
+    }
+}
